76e1b3279a74e451ad68f03dfc54f070b61b05a2
Usama Tahir <105447631+Glasoptik@users.noreply.github.com>
2022-08-19 17:06:31 +0500

    Update privatpolitikhandelsbetingelser.jsx

--- src/pages/privatpolitikhandelsbetingelser.jsx
+++ src/pages/privatpolitikhandelsbetingelser.jsx
@@ -11,13 +11,16 @@ const Policies = ({ data }) => {
   });
   return (
     <Layout>
-      <div className="max-w-[1440px] w-full overflow-x-visible flex justify-between box-border mx-auto px-5 sm:px-[74px] space-x-[66px] pt-4 mt-[105px] md:mt-40">
+      <div
+        className="max-w-[1440px] w-full overflow-x-visible flex flex-col sm:flex-row sm:justify-between box-border mx-auto px-5 sm:px-[74px] 
+      sm:space-x-[66px] pt-4 mt-[105px] md:mt-40"
+      >
         <div className="flex flex-col items-start max-w-max md:max-w-xs w-full space-y-2">
           {policies.policy.map((policy, index) => (
             <button
               onClick={() => setSelectedPolicy({ id: index, ...policy })}
               key={index}
-              className={`text-[15px] leading-6 font-medium ${
+              className={`text-xs sm:text-[15px] sm:leading-6 font-medium ${
                 selectedPolicy.id === index && "text-[#999999]"
               }`}
             >
@@ -25,8 +28,8 @@ const Policies = ({ data }) => {
             </button>
           ))}
         </div>
-        <div className="max-w-xl w-full">
-          <h1 className="font-medium text-[15px] leading-[18px] mb-5">
+        <div className="mt-10 sm:mt-0 sm:max-w-xl w-full">
+          <h1 className="font-medium text-[25px] sm:text-[15px] leading-[22px] sm:leading-[18px] mb-5">
             {selectedPolicy.title.text}
           </h1>
           <Reveal
